64c19a30d266764ce0a9618a58f9f34fae156656
RayCalitri <Raycalitri430@gmail.com>
2025-01-15 12:37:15 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,20 +1,41 @@
-const tickRate = 1000 / 30
+const tickRate = 1000 / 30; //30 fps
 let score = 0;
 
+let drill = new Building('Drill', 0.1, 15, 'buyDrill');
+let steel = new Building('steel', 0.1, 15, 'buysteel');
+let diamond = new Building('diamond', 1, 100, 'buydiamond');
 
-let woodenDrill = new Upgrade("woodenDrill", 0.1, 15, "buyEquipment");
+//let steeldrill = new Building('SteelDrill', 1, 100, 'buySteelDrill');
+//let diamonddrill = new Building('DiamondDrill', 5, 200, 'buyDiamondDrill');
+//let platinumdrill = new Building('PlatinumDrill', 10, 500, 'buyPlatinumDrill');
+
+function scorePlusPlus() {
+    score++;
+}
+
 function incScore() {
-    score += woodenDrill.cps
+    score += drill.cps;
+    score += steel.cps;
+    score += diamond.cps;
+    //score += steeldrill.cps;
+    //score += diamonddrill.cps;
+   // score += platinumdrill.cps;
 }
 
-function scorePlusPlus(){
-    score++
-    
-}
+function updateButtons() {
+    drill.buttonState();
+    steel.buttonState();
+    diamond.buttonState();
+    //steeldrill.buttonState();
+    //diamonddrill.buttonState();
+    //platinumdrill.buttonState();
 
-function updatePage (){
+}
+function updatePage() {
     incScore();
-    document.getElementById("score").innerHTML ="$" + Math.floor (score) .toLocaleString();
-
-    setInterval (updatePage, tickRate);
+    updateButtons();
+    document.getElementById('score').innerHTML =
+        '$' + Math.floor(score).toLocaleString();
 }
+
+setInterval(updatePage, tickRate);
--- src/upgrade.js
+++ src/upgrade.js
@@ -1,18 +1,45 @@
-class Upgrade {
-    constructor(upgradeName, baseCps, baseCost, buttonId){
-        this.upgradeName = upgradeName;
+class Building {
+    constructor(buildingName, baseCps, baseCost, buttonId) {
+        this.buildingName = buildingName;
         this.baseCps = baseCps / (1000 / tickRate);
-        this.Cost = baseCost;
+        this.baseCost = baseCost
+        this.cost = baseCost;
         this.buttonId = buttonId;
         this.amountOwned = 0;
-        this.cps =0;
-        this.visable = true ; //change to false 
+        this.cps = 0;
+        this.visible = false; // !!!!Change this to false!!!!!!
     }
 
-    purchase(){
+    purchase() {
         score -= this.cost;
-        this.amountOwned++
+        this.amountOwned++;
         this.cps = this.baseCps * this.amountOwned;
-        this.cost = Math.ceil(this.baseCost * 1.15 ** this.ammountOwned)
+        this.cost = Math.ceil(this.cost * 1.15 ** this.amountOwned);
+    }
+    buttonState(){
+        if (!this.visible){
+            document.getElementById(this.buttonId).style.display="none";
+            if (score >= this.baseCost){
+                this.visible = true;
+                document.getElementById(this.buttonId).style.display="initial";
+            }
+        }
+        
+        if (score < this.cost) {
+            document.getElementById(this.buttonId).disabled = true;
+        } else {
+            document.getElementById(this.buttonId).disabled = false;
+        }
+
+        document.getElementById(this.buttonId).innerHTML =
+            'Buy ' +
+            this.buildingName +
+            ' (Cost: $' +
+            Math.ceil(this.cost).toLocaleString() +
+            ') <br> Adds $' +
+            (this.baseCps * (1000 / tickRate)).toLocaleString() +
+            ' Per Second <br> [Owned: ' +
+            this.amountOwned +
+            ']';
     }
 }
